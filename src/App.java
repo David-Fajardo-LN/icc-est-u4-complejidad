cfaec2e6116acbd8b5e0da1832f2dbedfeb3520f
Your Name <you@example.com>
2026-04-10 12:49:17 -0500

    UPS: app file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int number = 10;
+        int [] array = {1,2,3,4,5};
+        System.out.println("Hello word");
+        /*//for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }*/
+        for (int i: array){
+            System.out.println(array[i]);
+        }
     }
 }

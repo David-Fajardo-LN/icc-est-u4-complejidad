e7d9f80ab2fa09f9a1a4928b45ed5e5da6de4fe3
David Fajardo <davidfajardo727@gmail.com>
2026-04-15 09:00:45 -0500

    Ejemplos

--- src/App.java
+++ src/App.java
@@ -1,14 +1,41 @@
+
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        int number = 10;
-        int [] array = {1,2,3,4,5};
-        System.out.println("Hello word");
-        System.out.println("El valor del numero es:" + number);
-        for (int i = 0; i < array.length; i++){
-            System.out.println(array[i]);
+        int cantidadEstudiantes = 20000000;
+        int cantidadConsultas = 40_000;
+
+        System.out.println("Generando Estudiantes....");
+        List<Estudiante> estudiantes = Generador.generarEstudiantes(cantidadEstudiantes);
+        //for (Estudiante est : estudiantes){
+        //    System.out.println(est.nombre);
+        //}
+
+        // BUSCAR SI EXISTE EL USR
+        // CON CI 1_999_993
+
+        String cedulaBuscar = "1999993";
+        for(Estudiante est : estudiantes){
+        if (est.cedula.equals(cedulaBuscar)){
+            System.out.println("EXISTE");
         }
-        for (int i: array){
-            System.out.println(array[i]);
         }
-    }
+
+        int inicio = 0;
+        int fin = estudiantes.size()-1;
+
+        while (inicio <= fin){
+            int medio = (inicio + fin)/2;
+            Estudiante actual = estudiantes.get(medio);
+            int comparacion = actual.cedula.compareTo(cedulaBuscar);
+            if(comparacion == 0){
+                System.out.println("EXISTE");break;
+            }else if (comparacion  < 0){
+                inicio = medio + 1;
+            }else{
+                fin = medio-1;
+            }
+        }
+    }       
 }
